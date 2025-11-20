007338232ea65db7de338032a1688a6869fed5bc
Mainakumarijat <promptguru98@gmail.com>
2025-11-20 21:25:56 +0530

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function About(){
+return (
+<section className="container mx-auto p-6 text-white">
+<h1 className="text-2xl">About Desiprompt</h1>
+<p className="mt-2 text-gray-300">Professional design and curated content.</p>
+</section>
+)
+}
