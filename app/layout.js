a0b3fd09220e00f4e982dc37d7590ff2c86511e6
Mainakumarijat <promptguru98@gmail.com>
2025-11-20 21:22:54 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+import './globals.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+
+export const metadata = {
+title: 'Desiprompt.in',
+description: 'Royal premium free images - Desiprompt'
+}
+
+
+export default function RootLayout({ children }) {
+return (
+<html lang="hi">
+<body>
+<Header />
+<main className="min-h-screen">{children}</main>
+<Footer />
+</body>
+</html>
+)
+}
